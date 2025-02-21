f1b7cff0363f8735f17168ac78862a107db5185d
Callum <callumcarroll70@gmail.com>
2025-02-21 08:45:36 +0000

    next batch

--- src/formHandler.php
+++ src/formHandler.php
@@ -1,48 +1,70 @@
-<html><h1>formhandler sent</h1></html>
 <?php
 
 require "./db.php";
+session_start();
 
-$userFetch = "SELECT * FROM users";
-$result = $conn->query($userFetch);
+if (!isset($_POST['action'])) {
+    setErrorAndRedirect("Invalid request.");
+}
 
-    if (isset($_POST['signIn'])){
-        echo "you signed in";
+switch ($_POST['action']) {
+    case 'signIn':
+        echo "You signed in";
+        break;
 
+    case 'signUp':
+        
 
-    }
-
-    if (isset($_POST['signUp'])){
-        echo "you signed up";
         $name = mysqli_real_escape_string($conn, $_POST['name']);
         $email = mysqli_real_escape_string($conn, $_POST['email']);
-            if ($_POST['password'] == $_POST['confirmedPassword']){
-            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-            
 
-            
-            $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
-
-            if (mysqli_query($conn, $sql)){
-                echo "You've signed up!";
-            }else{
-                session_start();
-                $_SESSION['error']= "Error: " . mysqli_error($conn);
-                header("Location: ./index.html");
-                exit();
-            }
-        }else{
-            session_start();
-            $_SESSION['error'] = "Passwords do not match";
-            header("Location: ./index.html");
-            exit();
+        if ($_POST['password'] !== $_POST['confirmedPassword']) {
+            setErrorAndRedirect("Passwords do not match");
         }
-    }    
-                
 
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-            
-    
+        // Check for extra fields and consent
+        $DOB = $genderAtBirth = null;
+        if (!empty($_POST['extra']) && !empty($_POST['consent'])) {
+            $DOB = mysqli_real_escape_string($conn, $_POST['DOB']);
+            $genderAtBirth = mysqli_real_escape_string($conn, $_POST['genderAtBirth']);
+        } elseif (!empty($_POST['extra']) && empty($_POST['consent'])) {
+            setErrorAndRedirect("You have chosen to submit more information but have not consented to the storage of it");
+        }
+
+        
+        insertUser($conn, $name, $email, $password, $DOB, $genderAtBirth);
+
+        $_SESSION['name'] = $name;
+        header("Location: ./homePage.php");
+        exit();
+
+    default:
+        setErrorAndRedirect("Invalid action.");
+}
 
 mysqli_close($conn);
- ?>
+
+function setErrorAndRedirect($message) {
+    $_SESSION['error'] = $message;
+    header("Location: ./index.php");
+    exit();
+}
+
+function insertUser($conn, $name, $email, $password, $DOB = null, $genderAtBirth = null) {
+    if ($DOB && $genderAtBirth) {
+        $sql = "INSERT INTO users (name, email, password, DOB, genderAtBirth) VALUES (?, ?, ?, ?, ?)";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "sssss", $name, $email, $password, $DOB, $genderAtBirth);
+    } else {
+        $sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "sss", $name, $email, $password);
+    }
+
+    if (!mysqli_stmt_execute($stmt)) {
+        setErrorAndRedirect("Error: " . mysqli_error($conn));
+    }
+}
+?>
